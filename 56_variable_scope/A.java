4644da9fe7bc5e68cd6fe3d93590996f62c0af22
Yash Kesharwani <101829844+yashkesharwani559@users.noreply.github.com>
2024-08-09 21:11:15 +0530

    Add files via upload

---
+++ 56_variable_scope/A.java
@@ -0,0 +1,6 @@
+class A{
+    int a = 34;
+    public static void main(){
+        int a = 23;
+    }
+}
---
+++ 56_variable_scope/B.java
@@ -0,0 +1,6 @@
+class B{
+    static int a = 45;
+    public static void main(String[] args){
+        int a = 23;
+    }
+}
